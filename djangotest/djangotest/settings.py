12ec91d0f2465a395b9f8d9caad9e9993900f29d
mandy <mandy@duomai.com>
2019-05-16 00:23:57 +0800

    update

--- djangotest/djangotest/settings.py
+++ djangotest/djangotest/settings.py
@@ -30,14 +30,16 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = {
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-]
+    #'web.apps.WebConfig',
+    'web',
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- djangotest/djangotest/urls.py
+++ djangotest/djangotest/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from web.views import Login
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/', Login)
 ]
